780ec54b8d5848eff3cf7a369af0bcd8b3e7c052
GorpoOrko <47964672+gorpo@users.noreply.github.com>
2020-06-19 21:26:07 -0300

    so mostra o 3d se clicado RealidadeAumentada

---
+++ codigo C# click unity/click.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class click : MonoBehaviour, Vuforia.IVirtualButtonEventHandler
+{
+
+    public GameObject esqueleto1, botao1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        botao1 = GameObject.Find("botao1");
+        botao1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        esqueleto1 = GameObject.Find("esqueleto1");
+        esqueleto1.SetActive(false);
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        esqueleto1.SetActive(true);
+        Debug.Log("BTN Pressed");
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        esqueleto1.SetActive(false);
+
+        Debug.Log("BTN Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
